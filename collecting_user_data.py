342220ccf6e26d2c401b3fa0a17167884f911386
mcastelli5 <matt.castelli2@gmail.com>
2022-07-25 14:21:43 -0400

    update user data file

--- collecting_user_data.py
+++ collecting_user_data.py
@@ -1,21 +1,26 @@
-def get_genres():
-  genres = input("What Movie Genre are you interested in (if multiple, please separate them with a comma)? [Type 'skip' to skip this question] ")
-  genres = " ".join(["".join(n.split()) for n in genres.lower().split(',')])
-  return genres
+class User:
+    def __init__(self):
+        self.name = None
+        self.search_terms = []
+        
+    def get_name(self):
+        self.name = input("Hi! What is your name?")
+    
+    def get_genres(self):
+        genres = input("What Movie Genre are you interested in (if multiple, please separate them with a comma)? [Type 'skip' to skip this question] ")
+        genres = " ".join(["".join(n.split()) for n in genres.lower().split(',')])
+        return genres
 
-def get_keywords():
-  keywords = input("What are some of the keywords that describe the movie you want to watch, like elements of the plot, whether or not it is about friendship, etc? (if multiple, please separate them with a comma)? [Type 'skip' to skip this question] ")
-  keywords = " ".join(["".join(n.split()) for n in keywords.lower().split(',')])
-  return keywords
+    def get_keywords(self):
+        keywords = input("What are some of the keywords that describe the movie you want to watch, like elements of the plot, whether or not it is about friendship, etc? (if multiple, please separate them with a comma)? [Type 'skip' to skip this question] ")
+        keywords = " ".join(["".join(n.split()) for n in keywords.lower().split(',')])
+        return keywords
 
-def get_searchTerms():
-  searchTerms = [] 
-  genres = get_genres()
-  if genres != 'skip':
-    searchTerms.append(genres)
+    def get_searchTerms(self):
+        genres = self.get_genres()
+        if genres != 'skip':
+            self.search_terms.append(genres)
 
-  keywords = get_keywords()
-  if keywords != 'skip':
-    searchTerms.append(keywords)
-  
-  return searchTerms
+        keywords = self.get_keywords()
+        if keywords != 'skip':
+            self.search_terms.append(keywords)
