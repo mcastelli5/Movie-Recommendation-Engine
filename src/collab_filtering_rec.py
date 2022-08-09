51590b457044e7ac11bae4dbd473eb4be538ac13
mcastelli5 <matt.castelli2@gmail.com>
2022-08-09 12:36:12 -0400

    updates to top 10 file

--- src/collab_filtering_rec.py
+++ src/collab_filtering_rec.py
@@ -2,13 +2,7 @@ from get_estimate import get_estimate
 import config
 import joblib
 
-import pandas as pd
-
-from surprise import Dataset
-from surprise import Reader
-from surprise.model_selection import cross_validate
-
-def collab_based_rec(): #movies, ratings, fav_movie
+def collab_based_rec(movies, ratings, fav_movie):
     """
     Summary:
         provide user with recommended movies based on collaborative filtering approach by using their favorite movie to find similar users and their respective preferences.
@@ -20,7 +14,8 @@ def collab_based_rec(): #movies, ratings, fav_movie
         
     Returns:
         list: collaborative filtered movie recommendations based on user's favorite movie
-
+    """
+    
     model = joblib.load(config.SVD_MODEL)
     df = movies.copy(deep=True)
     
@@ -32,16 +27,3 @@ def collab_based_rec(): #movies, ratings, fav_movie
 
     # Estimate the ratings the User would give each movie based on the similar users (predicted) ratings
     df['est'] = df.apply(lambda x: get_estimate(similar_users, x.id, model), axis=1)
-    """
-    model = joblib.load(config.SVD_MODEL)
-    # Ingest all inputs
-    ratings = pd.read_csv(config.TRAINING_FILE_RATINGS)
-    
-    # Instantiate reader object and svd object
-    reader = Reader(rating_scale=(1, 5))
-
-    # Load data into surprise dataset format with reader
-    data = Dataset.load_from_df(ratings[['userId', 'movieId', 'rating']], reader)
-    cross_validate(model, data, measures=['RMSE', 'MAE'], cv=5, verbose=True)
-    print(model)
-    return model
--- src/top_movies_rec.py
+++ src/top_movies_rec.py
@@ -1,4 +1,6 @@
-def top_movies_rec(movies):
+import config
+
+def top_movies_rec():
     """
     Summary:
         provides user with top 10 most popular movies at this moment, since they did not provide any input. Not personalized towards the user at all.
@@ -13,6 +15,7 @@ def top_movies_rec(movies):
         user just wants to see the most popular movies without a personalized recommendation algorithm added to it
         user accidentally bypassess the user preferences and scrolls to the movie recs section which will automatically populate the top movies at the moment (static for this project)
     """
+    movies = config.TRAINING_FILE_MOVIES
     ranked_titles = []
     ranked_movies = movies.copy(deep=True)
     ranked_movies = ranked_movies.sort_values("popularity", ascending=False)
