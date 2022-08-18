b112945304fdebcd337bc41f4856a39b7dd66b71
mcastelli5 <matt.castelli2@gmail.com>
2022-08-18 10:34:08 -0400

    frontend fix

--- src/config.py
+++ src/config.py
@@ -1,7 +1,7 @@
 TRAINING_FILE_MOVIES = "data/movies.csv"
-TRAINING_FILE_RATINGS = "../data/ratings_small.csv"
-TRAINING_FILE_LINKS = "../data/links_small.csv"
+TRAINING_FILE_RATINGS = "data/ratings_small.csv"
+TRAINING_FILE_LINKS = "data/links_small.csv"
 
-MODEL_OUTPUT = "../model/"
+MODEL_OUTPUT = "model/"
 
-SVD_MODEL = "../model/svd.bin"
+SVD_MODEL = "model/svd.bin"
